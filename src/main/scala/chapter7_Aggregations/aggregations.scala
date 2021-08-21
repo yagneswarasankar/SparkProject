735e24337b03b76e2327a406ec3e13e81ee54c53
Girijasankar <yagneswarasankar@gmail.com>
2021-08-21 22:31:11 +0530

    Chapter 7 , Dataframe level aggregations

--- src/main/scala/chapter7_Aggregations/aggregations.scala
+++ src/main/scala/chapter7_Aggregations/aggregations.scala
@@ -2,6 +2,7 @@ package chapter7_Aggregations
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{approx_count_distinct, collect_list, collect_set, count, countDistinct, first, last, max, min, sum, sumDistinct}
 
 object aggregations {
 
@@ -16,7 +17,45 @@ object aggregations {
 
     val rtlData = spark.read.format("csv").option("header","true").option("inferSchema","true")
       .load("src/main/resources/simple/retail-data/all/*")
-    rtlData.show(10)
+
+    /************************************************************************************
+     *                                         Count
+     ************************************************************************************/
+
+    rtlData.select(count("stockcode").alias("count"))
+      .show(truncate = false)
+
+    rtlData.select(countDistinct("stockcode").alias("contdistinct"))
+      .show(truncate = false)
+
+    rtlData.select(approx_count_distinct("stockcode",0.1).as("approximatecount")).show()
+
+    rtlData.select(first("stockcode"),last("stockcode"))
+      .show(truncate = false)
+
+    rtlData.select(min("quantity"),max("quantity")).show()
+
+
+    rtlData.select(sum("quantity").as("sum"),
+                   sumDistinct("quantity").as("sumDisinct"))
+                  .show()
+
+
+
+    /***********************************************************************************
+     * Aggregating complex data types
+     ***********************************************************************************/
+
+    rtlData.agg(collect_set("country"),collect_list("country"))
+      .show()
+
+
+
+
+
+
+
+
 
 
 

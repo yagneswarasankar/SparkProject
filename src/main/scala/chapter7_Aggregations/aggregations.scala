76d712b633d622ba820582132e659faa494c0b6c
Girijasankar <yagneswarasankar@gmail.com>
2021-08-24 17:29:09 +0530

    Chapter 8 - Aggregations

--- src/main/scala/chapter7_Aggregations/aggregations.scala
+++ src/main/scala/chapter7_Aggregations/aggregations.scala
@@ -2,7 +2,8 @@ package chapter7_Aggregations
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{approx_count_distinct, collect_list, collect_set, count, countDistinct, first, last, max, min, sum, sumDistinct}
+import org.apache.spark.sql.functions.{col, grouping_id, sum, to_date}
+//import org.apache.spark.sql.functions.{approx_count_distinct, collect_list, collect_set, count, countDistinct, first, last, max, min, sum, sumDistinct}
 
 object aggregations {
 
@@ -17,7 +18,7 @@ object aggregations {
 
     val rtlData = spark.read.format("csv").option("header","true").option("inferSchema","true")
       .load("src/main/resources/simple/retail-data/all/*")
-
+/*
     /************************************************************************************
      *                                         Count
      ************************************************************************************/
@@ -52,15 +53,94 @@ object aggregations {
 
 
 
+    /***********************************************************************************
+    *                                       Grouping
+    ************************************************************************************/
+
+    rtlData.groupBy("InvoiceNo","CustomerID").count().show(false)
+    rtlData.groupBy("InvoiceNo").agg(
+      count("quantity").as("quantity"),
+       expr("count(quantity)"))
+      .show(4,false)
 
 
 
+    //2010-12-01 08:26:00
+
+    val forWindoFuncSpec = rtlData.withColumn("date",to_date(col("InvoiceDate"),"MM/d/yyyy H:mm"))
+    val windowSpec = Window
+      .partitionBy("CustomerID","date")
+      .orderBy(col("quantity").desc)
+      .rowsBetween(Window.unboundedPreceding,Window.currentRow)
 
 
+    val maxpurchaseCustomer = max(col("quantity")).over(windowSpec)
+    val denseRank = dense_rank().over(windowSpec)
+    val rank1  = rank().over(windowSpec)
 
+    forWindoFuncSpec.select(col("customerID"),col("date"),col("quantity"),col("Description"),
+      denseRank.as("denseRank"),
+      rank1.as("rank1"),
+      maxpurchaseCustomer.alias("maxpurchase")).show(false)
+
+ */
+    /***********************************************************************
+     *                             Rollup and Cube and group_id
+     ***********************************************************************/
+
+
+    val rtlDatanotNull = rtlData.withColumn("date",to_date(col("InvoiceDate"),"MM/d/yyyy H:mm")).drop()
+
+    rtlDatanotNull.rollup("date","country").agg(sum("quantity").as("sumQty"))
+      .select(col("date"),col("country"),col("sumqty"))
+      .orderBy("date")
+      //.show()
+
+    rtlDatanotNull.cube("date","country").agg(sum("quantity").as("sumQty"))
+      .select(col("date"),col("country"),col("sumqty"))
+      .orderBy("date")
+      //.show(100,truncate = false)
+
+    rtlDatanotNull.cube("date","country").agg(grouping_id().as("grpid"),sum("quantity").as("sumQty"))
+      .select(col("date"),col("country"),col("grpid"),col("sumqty"))
+      .orderBy("date")
+      //.show(100,truncate = false)
+
+    /**********************************************************************************
+     *                                  pivot
+     ******************************************************************************/
+
+    val rtlDatanotpivot = rtlData.withColumn("date",to_date(col("InvoiceDate"),"MM/d/yyyy H:mm")).drop()
+
+    rtlDatanotpivot.where(col("country") === "USA").groupBy("date").pivot("country").sum()
+      //.show()
+
+    import spark.implicits._
+    val studentDF  = Seq(
+      ("Girija","Maths",100),
+      ("Girija","Science",93),
+      ("Hari","Maths",100),
+      ("Hari","Science",94)).toDF("name","subject","marks")
+
+    studentDF.groupBy("name").pivot("subject").sum("marks")
+     // .show()
+
+    studentDF.groupBy("name").pivot("subject").avg("marks")
+      .show()
+
+    /*************************
+     * output - Rows turned into Columns
+     * +------+-----+-------+
+     * |  name|Maths|Science|
+     * +------+-----+-------+
+     * |  Hari|100.0|   94.0|
+     * |Girija|100.0|   93.0|
+     * +------+-----+-------+
+     */
 
 
 
   }
+
 
 }
---
+++ src/main/scala/chapter8_Joins/Joins.scala
@@ -0,0 +1,101 @@
+package chapter8_Joins
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.expr
+
+object Joins {
+
+  def main(args: Array[String]):Unit = {
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
+
+    import spark.implicits._
+    val person = Seq(
+      (0, "Bill Chambers", 0, Seq(100)),
+      (1, "Matei Zaharia", 1, Seq(500, 250, 100)),
+      (2, "Michael Armbrust", 1, Seq(250, 100)))
+      .toDF("id", "name", "graduate_program", "spark_status")
+    val graduateProgram = Seq(
+      (0, "Masters", "School of Information", "UC Berkeley"),
+      (2, "Masters", "EECS", "UC Berkeley"),
+      (1, "Ph.D.", "EECS", "UC Berkeley"))
+      .toDF("id", "degree", "department", "school")
+    val sparkStatus = Seq(
+      (500, "Vice President"),
+      (250, "PMC Member"),
+      (100, "Contributor"))
+      .toDF("id", "status")
+
+    /***************************************************
+     *                   Inner Join
+     ***************************************************/
+
+    var joinType = "inner"
+    val joinDFCond = person.col("graduate_program") === graduateProgram.col("id")
+    /*person.join(graduateProgram,joinDFCond,joinType)
+      .show(false)
+
+    /******************************************************************
+     *                             Outer Joins
+     ******************************************************************/
+
+    joinType = "outer"
+    person.join(graduateProgram,joinDFCond,joinType)
+      .show(false)
+
+    joinType = "left_outer"
+    graduateProgram.join(person,joinDFCond,joinType)
+      .show(false)
+
+    joinType = "right_outer"
+    person.join(graduateProgram,joinDFCond,joinType)
+      .show(false)
+
+    /**********************************************************
+     *                    Semi Join
+     *********************************************************/
+
+    joinType = "left_semi"
+    graduateProgram.join(person,joinDFCond,joinType)
+      //.show()
+
+    val gradProgram2 = graduateProgram.union(Seq(
+      (0, "Masters", "Duplicated Row", "Duplicated School")).toDF())
+    gradProgram2.join(person,joinDFCond,joinType)
+      //.show()
+
+    /*****************************************************************
+     * left_anti
+     ****************************************************************/
+    joinType = "left_anti"
+    graduateProgram.join(person,joinDFCond,joinType)
+      .show()
+
+    /**************************************************************
+     * Cross Join
+     **************************************************************/
+    person.crossJoin(graduateProgram)
+      .show()
+
+     */
+
+    /******************************************************************
+     * Join with complex type
+     */
+
+    person.withColumnRenamed("id","personid")
+      .join(sparkStatus,expr(" array_contains(spark_Status,id)"))
+      .show()
+
+    person.withColumnRenamed("id","personid")
+      .join(sparkStatus,expr("not array_contains(spark_Status,id)"))
+      .show()
+
+
+  }
+}

7f80431fbc627a02c36aac7d17d7a424ee1be8c9
Girijasankar <yagneswarasankar@gmail.com>
2021-08-28 22:17:54 +0530

    Chapter 9 - Connecting to various RDBMSs from Spark

--- src/main/scala/chapter9_Datasource/DifferentRDBMS.scala
+++ src/main/scala/chapter9_Datasource/DifferentRDBMS.scala
@@ -2,7 +2,6 @@ package chapter9_Datasource
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.col
 
 object DifferentRDBMS {
 
@@ -16,6 +15,10 @@ object DifferentRDBMS {
 
     val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
 
+
+    /**************************************************************************
+     * sqlite
+     **************************************************************************/
     val jdbcDriver  = "org.sqlite.JDBC"
     val path1  = "src/main/resources/simple/flight-data/jdbc/my-sqlite.db"
     val url = s"jdbc:sqlite:${path1}"
@@ -28,7 +31,7 @@ object DifferentRDBMS {
     val tabData = spark.read.format("jdbc").option("url",url)
       .option("dbtable",table_name).option("driver",jdbcDriver).load()
 
-    tabData.show()
+    //tabData.show()
 
     /********************************************************
      * Oracle
@@ -37,18 +40,67 @@ object DifferentRDBMS {
    //Localhost to be replaced with ip address and the passworod also has to be changed.
 
     val jdbcOracleDriver  = "oracle.jdbc.driver.OracleDriver"
-    val oracleUrl = s"jdbc:oracle:thin:girija/<Password>@<IPADDRESS>:1521:XE"
+    val oracleUrl = s"jdbc:oracle:thin:girija/girija@192.168.61.1:1521:XE"
     val oracleTableName = "GIRIJA.emp"
 
     val oracleDt = spark.read.format("jdbc")
       .option("driver",jdbcOracleDriver)
       .option("url",oracleUrl)
       .option("dbtable",oracleTableName)
-      //.option("user","girija")
-      //.option("password","password")
       .load()
 
-    oracleDt.where(col("JOB") === "MA").explain()
+    //oracleDt.where(col("JOB") === "MA").explain()
+
+    /**************************************************************************
+     *                                       postgres
+     **************************************************************************/
+
+    val jdbcpostgresDriver = "org.postgresql.Driver"
+    val postgresconnectionString = "jdbc:postgresql:testdb"//192.168.61.1"
+    val postgresTableName = "company"
+
+    val postgresDF = spark.read.format("jdbc")
+      .option("url",postgresconnectionString)
+      .option("driver",jdbcpostgresDriver)
+      .option("user","postgres")
+      .option("password","girija")
+      .option("dbtable",postgresTableName)
+      .load()
+
+     /*val connection = DriverManager.getConnection(url)
+        connection.isClosed
+        connection.close()*/
+     //postgresDF.show(false)
+
+    /*
+    +---+-----+---+--------------------------------------------------+-------+----------+
+    |id |name |age|address                                           |salary |join_date |
+    +---+-----+---+--------------------------------------------------+-------+----------+
+    |1  |Paul |32 |California                                        |20000.0|2001-07-13|
+    |2  |Allen|25 |Texas                                             |null   |2007-12-13|
+    |3  |Teddy|23 |Norway                                            |20000.0|null      |
+    +---+-----+---+--------------------------------------------------+-------+----------+
+     */
+
+    /**********************************************************************
+     *                                Mysql
+     **********************************************************************/
+
+    val mysqlDriver = "com.mysql.jdbc.Driver"
+    val mysqlUrl = "jdbc:mysql://192.168.61.1:3306/sakila"
+    val tblname = "country"
+
+    val mysqlDF = spark.read.format("jdbc")
+      .option("url",mysqlUrl)
+      .option("driver",mysqlDriver)
+      .option("dbtable",tblname)
+      .option("user","root")
+      .option("password","girija")
+      .load()
+    mysqlDF.show()
+
+
+
 
   }
 }
